32b563ef8c6ce890ac60e3df9d4e05ac2c150dd7
JoseMarin <jmarinmalagon@gamil.com>
2022-04-22 11:42:31 +0200

    ejemplo 04 22

---
+++ mysite/api_bookstore/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .serializers import AuthorSerializer, BookSerializer
+from .models import Author, Book
+
+# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all().order_by('title')
+    serializer_class = BookSerializer
+
+@api_view(["GET"])
+@csrf_exempt
+@permission_classes([IsAuthenticated])
+def welcome(request):
+    content = {"message": "Welcome to the BookStore!"}
+    return JsonResponse(content)
